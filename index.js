078f38fd4eb7abfbb5a1935e458c23e233a9668e
Ashlin Innis <754360@student.tor.loc>
2019-08-14 13:42:55 -0400

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 var express = require('express');
 var app = express();
-app.get('/',function(req,res){
-    res.send(' hello everyone')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(function (req,res,next){
+    console.log("i amin the middle");
+    console.log(req.body);
+    next();
+    
 
+    })
+app.get('/',function (req,res){
+    res.send('<h1>get hello world</h1>')
+});
+app.post('/',function (req,res){
+    console.log(reg.body)
+    res.send('<h1>post sent</h1>')
+});
+app.put('/',function (req,res){
+    res.send('PUT send')
+});
+app.delete('/',function (req,res){
+    res.send('DELETE sent')
 });
 app.listen(3000);
